99c3896cdbed9971987530ab847e880911ff5f62
Apple-Kong <kongthn@gmail.com>
2023-09-03 01:20:43 +0900

    feat my entry screen

--- lib/screen/my_entry_screen.dart
+++ lib/screen/my_entry_screen.dart
@@ -18,12 +18,198 @@ class MyEntryScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0.1,
       ),
-      body: const Column(
-        children: [
-          Row(
-            children: [],
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 55,
+                      height: 55,
+                      decoration: ShapeDecoration(
+                        image: const DecorationImage(
+                          image:
+                              NetworkImage("https://via.placeholder.com/55x55"),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '공태현',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1.10,
+                          ),
+                        ),
+                        Text(
+                          '010-0000-0000',
+                          style: TextStyle(
+                            color: Color(0xFF8E8E93),
+                            fontSize: 15,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w500,
+                            height: 1.47,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '접수 현황',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w600,
+                      height: 1,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  ListView.builder(
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return const MyEntryCard();
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyEntryCard extends StatelessWidget {
+  const MyEntryCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(
+            width: 1,
+            strokeAlign: BorderSide.strokeAlignOutside,
+            color: Color(0xFFE5E5EA),
+          ),
+          borderRadius: BorderRadius.circular(9),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text(
+            'RESPECT Dance Battle vol.1',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w600,
+              height: 1,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const Text(
+            '2023.04.25  / 15:00',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 17,
+              fontFamily: 'Pretendard',
+              fontWeight: FontWeight.w400,
+              height: 1.29,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            decoration: ShapeDecoration(
+              color: const Color(0xFFF7F7F7),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8)),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '왁킹 사이드 배틀 참가',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                        height: 1.29,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '184-89532789',
+                      style: TextStyle(
+                        color: Color(0xFF636366),
+                        fontSize: 15,
+                        fontFamily: 'Pretendard',
+                        fontWeight: FontWeight.w500,
+                        height: 1.47,
+                      ),
+                    )
+                  ],
+                ),
+                Spacer(),
+                Text(
+                  'KRW 30,000',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w500,
+                    height: 1.29,
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ]),
       ),
     );
   }
