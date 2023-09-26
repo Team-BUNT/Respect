6b6d4456c7463ebbd7df79d81d4b4722d6963c3b
Apple-Kong <kongthn@gmail.com>
2023-09-26 13:14:21 +0900

    [Feat] sign up agreement

--- lib/screen/sign_in_add_info_screen.dart
+++ lib/screen/sign_in_add_info_screen.dart
@@ -5,6 +5,7 @@ import 'package:respect/components/ht_button.dart';
 import 'package:respect/components/ht_text_field.dart';
 import 'package:respect/constants.dart';
 
+import '../components/checkbox_term_list_tile.dart';
 import '../components/ht_dropdown.dart';
 
 class SignInAddInfoScreen extends StatefulWidget {
@@ -20,6 +21,9 @@ class _SignInAddInfoScreenState extends State<SignInAddInfoScreen> {
   String? year;
   String? month;
   String? day;
+  bool agreePrivacy = false;
+  bool agreePurchase = false;
+  bool agreeMarketing = false;
 
   final List<String> years = List.generate(100, (index) {
     final num = DateTime.now().year - index;
@@ -56,7 +60,7 @@ class _SignInAddInfoScreenState extends State<SignInAddInfoScreen> {
             ),
             const SizedBox(height: 12),
             HTTextField(
-              hintText: "댄서 네임",
+              hintText: "댄서 네임 (행사 참가시 사용됩니다.)",
             ),
             const SizedBox(height: 12),
             Row(children: [
@@ -103,6 +107,48 @@ class _SignInAddInfoScreenState extends State<SignInAddInfoScreen> {
               ),
             ]),
             const Spacer(),
+            //MARK: - 이용 약관 동의
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '이용 약관 동의',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                    fontFamily: 'Pretendard',
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                checkAllAgree(),
+                const SizedBox(height: 16.0),
+                CheckboxTermListTile(
+                  value: agreePrivacy,
+                  title: '[필수] 이용약관',
+                  onChanged: (newValue) {
+                    setState(() => agreePrivacy = newValue!);
+                  },
+                ),
+                const SizedBox(height: 4.0),
+                CheckboxTermListTile(
+                  value: agreePurchase,
+                  title: '[필수] 개인정보 수집, 이용 동의서',
+                  onChanged: (newValue) {
+                    setState(() => agreePurchase = newValue!);
+                  },
+                ),
+                const SizedBox(height: 4.0),
+                CheckboxTermListTile(
+                  value: agreeMarketing,
+                  title: '[선택] 마케팅 정보 수신동의',
+                  onChanged: (newValue) {
+                    setState(() => agreeMarketing = newValue!);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 32),
             HTButton(
               title: "회원 가입",
               titleColor: Colors.white,
@@ -112,6 +158,68 @@ class _SignInAddInfoScreenState extends State<SignInAddInfoScreen> {
           ],
         ),
       )),
+    );
+  }
+
+  Widget checkAllAgree() {
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      pressedOpacity: 1.0,
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(
+              color: Color(0xFFCCCCCC),
+            ),
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              if (agreePrivacy && agreePurchase && agreeMarketing)
+                Image.asset(
+                  'asset/icons/checkmark_circle_fill.png',
+                  width: 20.0,
+                  height: 20.0,
+                )
+              else
+                Padding(
+                  padding: EdgeInsets.zero,
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: const ShapeDecoration(
+                      shape: CircleBorder(
+                        side: BorderSide(color: Color(0xFFCCCCCC), width: 1.0),
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(width: 12.0),
+              const Text(
+                '전체동의',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      onPressed: () {
+        final newValue = (agreePrivacy && agreePurchase && agreeMarketing);
+
+        setState(() {
+          agreePrivacy = !newValue;
+          agreePurchase = !newValue;
+          agreeMarketing = !newValue;
+        });
+      },
     );
   }
 }
